cfa99aa31453e24cd655de2560b79749b1af503e
Enata <emmanueltag123@gmail.com>
2024-07-06 19:46:28 +0000

    Add and Update application icon

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,10 +2,6 @@ class Constants {
   Constants._();
 
   static const apiKey = "INSERT YOUR API KEY HERE";
-
-  // Test Api key
-  // static const apiKey ="539a0b569a029f741edbdf10e26e03c4";
-
   static const baseUrl = "https://openweathermap.org/img/wn/";
   static const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
   static const forecastBaseUrl = "https://api.openweathermap.org/data/2.5/forecast";
